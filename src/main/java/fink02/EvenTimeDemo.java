7d34b6360feaf7db7f6b292a5d132a2a7cf2aca7
a524631266 <524631266@qq.com>
2020-09-29 20:33:53 +0800

    start eventtime

---
+++ src/main/java/fink02/EvenTimeDemo.java
@@ -0,0 +1,17 @@
+package fink02;
+
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class EvenTimeDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+//        env.addSource(new FlinkKafkaConsumer09())
+        
+
+        env.execute();
+    }
+}
