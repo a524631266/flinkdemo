7c57533917cda2f8ee339ac2dd0e65eef7264fc1
a524631266 <524631266@qq.com>
2020-10-26 18:21:47 +0800

    first Join

---
+++ src/main/java/flinkbase/stream/JoinDemo.java
@@ -0,0 +1,12 @@
+package flinkbase.stream;
+
+import flinkbase.utils.EnvUtil;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class JoinDemo {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment localWebEnv = EnvUtil.getLocalWebEnv();
+
+//        localWebEnv.addSource()
+    }
+}
