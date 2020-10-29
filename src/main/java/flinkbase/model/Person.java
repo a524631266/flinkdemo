d6871fa12a90bc1e46acc27daa9d0a01ab8aaa1c
Zhang Liangliang <zhangll@homewell.com.cn>
2020-10-29 13:37:38 +0800

    修改string数组中只有一个num的情况

--- src/main/java/flinkbase/model/Person.java
+++ src/main/java/flinkbase/model/Person.java
@@ -1,5 +1,6 @@
 package flinkbase.model;
 
+import com.zhangll.flink.annotation.FieldTokenType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Person{
+    @FieldTokenType(min = "30" , max = "50")
     int age;
+    @FieldTokenType(value = {"张三", "李四" ,"王五" , "赵六"})
+    String name;
     Address address;
 }
 
