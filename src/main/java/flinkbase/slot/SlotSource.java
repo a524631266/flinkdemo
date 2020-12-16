6f604a35447437c5fc434fc914be7e0f425df08f
Zhang Liangliang <zhangll@homewell.com.cn>
2020-12-16 16:35:07 +0800

    slot相关源码分析

---
+++ src/main/java/flinkbase/slot/SlotSource.java
@@ -0,0 +1,9 @@
+package flinkbase.slot;
+
+import org.apache.flink.runtime.taskexecutor.slot.TaskSlotTableImpl;
+
+/**
+ * @see TaskSlotTableImpl#TaskSlotTableImpl(int, org.apache.flink.runtime.clusterframework.types.ResourceProfile, org.apache.flink.runtime.clusterframework.types.ResourceProfile, int, org.apache.flink.runtime.taskexecutor.slot.TimerService)
+ */
+public class SlotSource {
+}
